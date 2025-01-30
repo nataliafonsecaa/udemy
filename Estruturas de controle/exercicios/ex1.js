ce9b383a1e0741812f81c12628a4013652ceca6a
Natalia <natalia.fonseca8@etec.sp.gov.br>
2025-01-29 22:08:44 -0300

    up

---
+++ Estruturas de controle/exercicios/ex1.js
@@ -0,0 +1,28 @@
+// console.log('contar de 1 a 10');
+
+
+// for (let i =0; i<=10; i++)
+//     console.log(`i * ${i}`);
+    
+// console.log('tabuada');
+
+// let numero = 8;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${numero} x ${i} = ${numero * i}`);
+// }
+
+// console.log('contagem regressiva');
+// let i = 10;
+// while (i >= 0) {
+//   console.log(i);
+//   i--;
+// }
+
+// let a = 0, b = 1, temp;
+// console.log(a);
+// console.log(b);
+
+
+
+
+
